60639693fa12c48c9764a9637020b8d4049f3a87
Jinkalbhamani <167600910+Jinkalbhamani@users.noreply.github.com>
2024-12-03 12:09:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  ScrollReveal().reveal(".content h1 ", {
+    ...scrollRevealOption,
+  });
+
+  ScrollReveal().reveal(".content h2 ", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal(".content p", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+
+  ScrollReveal().reveal(".container", {
+    ...scrollRevealOption,
+  });
+
+  ScrollReveal().reveal(".content", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  ScrollReveal().reveal(".container3", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".card-container", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  
+  ScrollReveal().reveal(".collection", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+
+  ScrollReveal().reveal(".images", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".slideshow", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+
+  ScrollReveal().reveal(".show", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+
+  ScrollReveal().reveal(".head", {
+    duration: 1000,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".homeshop", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".cards", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+
+  ScrollReveal().reveal(".box-container", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".service__grid", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+
+
+  ScrollReveal().reveal(".row", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".item", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".main-text", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".rowcon", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  ScrollReveal().reveal(".footerContainer", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+ 
+  ScrollReveal().reveal(".footerBottom", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+
+  
+
+
+  
